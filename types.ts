88569551a0ef035373a52e70be9ae7327183df4b
BboyStatix <statix231@hotmail.com>
2025-08-12 16:33:45 +0800

    add prettier and format repo

--- types.ts
+++ types.ts
@@ -1,97 +1,104 @@
 export type Response<T> = {
-    status: number;
-    data: T;
-  }
-  export type ErrorResponse = {
-    code: number;
-    message: string;
-  }
-  
-  export type GeneralResponse = {
-    code: number;
-    message: string;
-  }
-  
-  type Network = "testnet" | "mainnet" | "cyan" | "aqua" | "celeste";
-  type Chain = "evm" | "solana" | "casper" | "other";
-  
-  export type Team = {
-    id: number;
-    name: string;
-    display_name: string;
-  }
-  export type GetOrganizationsResponse = {
-    teams: Team[];
-  }
-  
-  export type Project = {
-    name: string;
-    network: Network;
-    project_id: string;
-    project_secret: string;
-    user_id: string;
-    team_id: number;
-  };
-  
-  type PlatformType = "web" | "react_native" | "android" | "ios" | "flutter" | "unreal" | "unity";
-  
-  export type GetProjectResponse = {
-    project: Project;
-  }
-  
-  export type GetProjectsResponse = {
-    projects: Project[];
-  }
-  
-  export type CreateProjectRequest = {
-    name: string;
-    network: Network;
-    chain_namespaces: [
-        {
-            chain_namespace: Chain;
-            wallet_permission: boolean
-        },
-    ],
-    products?: string[];
-    platform_types: PlatformType[];
-    team_id: number;
-  }
-  export type CreateProjectResponse = {
-    message: string;
-    project_id: string;
-  }
-  export type ChainConfigRequest = {
-    id?: Number; //for updating an existing chain
-    chain_namespace: Chain;
-    wallet_permission?: boolean;
-    is_deleted?: boolean;
-  }
-  export type UpdateProjectRequest = {
-    name: string;
-    network: Network;
-    chain_namespaces?: ChainConfigRequest[];
-    platform_types?: PlatformType[];
-    team_id: number;
-  }
-  
-  export type ChainNamespace = {
-    ID: number;
-    chain_namespace: Chain;
-    wallet_permission: boolean;
-    project_id: string;
-  }
-  export type GetChainNamespacesResponse = {
-    chain_namespaces: ChainNamespace[];
-  }
-  
-  export type CreateWhitelistRequest = {
-    url: string;
-    team_id: number;
-  }
-  export type GetWhitelistsResponse = {
-    urls: string[] | null;
-    signed_urls: Record<string, string>;
-  }
-  export type DeleteWhitelistRequest = {
-    url: string;
-  }
+  status: number;
+  data: T;
+};
+export type ErrorResponse = {
+  code: number;
+  message: string;
+};
+
+export type GeneralResponse = {
+  code: number;
+  message: string;
+};
+
+type Network = "testnet" | "mainnet" | "cyan" | "aqua" | "celeste";
+type Chain = "evm" | "solana" | "casper" | "other";
+
+export type Team = {
+  id: number;
+  name: string;
+  display_name: string;
+};
+export type GetOrganizationsResponse = {
+  teams: Team[];
+};
+
+export type Project = {
+  name: string;
+  network: Network;
+  project_id: string;
+  project_secret: string;
+  user_id: string;
+  team_id: number;
+};
+
+type PlatformType =
+  | "web"
+  | "react_native"
+  | "android"
+  | "ios"
+  | "flutter"
+  | "unreal"
+  | "unity";
+
+export type GetProjectResponse = {
+  project: Project;
+};
+
+export type GetProjectsResponse = {
+  projects: Project[];
+};
+
+export type CreateProjectRequest = {
+  name: string;
+  network: Network;
+  chain_namespaces: [
+    {
+      chain_namespace: Chain;
+      wallet_permission: boolean;
+    },
+  ];
+  products?: string[];
+  platform_types: PlatformType[];
+  team_id: number;
+};
+export type CreateProjectResponse = {
+  message: string;
+  project_id: string;
+};
+export type ChainConfigRequest = {
+  id?: Number; //for updating an existing chain
+  chain_namespace: Chain;
+  wallet_permission?: boolean;
+  is_deleted?: boolean;
+};
+export type UpdateProjectRequest = {
+  name: string;
+  network: Network;
+  chain_namespaces?: ChainConfigRequest[];
+  platform_types?: PlatformType[];
+  team_id: number;
+};
+
+export type ChainNamespace = {
+  ID: number;
+  chain_namespace: Chain;
+  wallet_permission: boolean;
+  project_id: string;
+};
+export type GetChainNamespacesResponse = {
+  chain_namespaces: ChainNamespace[];
+};
+
+export type CreateWhitelistRequest = {
+  url: string;
+  team_id: number;
+};
+export type GetWhitelistsResponse = {
+  urls: string[] | null;
+  signed_urls: Record<string, string>;
+};
+export type DeleteWhitelistRequest = {
+  url: string;
+};
